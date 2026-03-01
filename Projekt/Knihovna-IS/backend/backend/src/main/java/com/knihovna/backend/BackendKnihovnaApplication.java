34efd43ecbabc8a37b6096041c37bccef13efd83
vojtechkubicek <vojtakubicek6@gmail.com>
2026-03-02 00:41:51 +0100

    Add files via upload

---
+++ Projekt/Knihovna-IS/backend/backend/src/main/java/com/knihovna/backend/BackendKnihovnaApplication.java
@@ -0,0 +1,13 @@
+package com.knihovna.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKnihovnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKnihovnaApplication.class, args);
+	}
+
+}
---
+++ Projekt/Knihovna-IS/backend/backend/src/test/java/com/knihovna/backend/BackendKnihovnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knihovna.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKnihovnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
